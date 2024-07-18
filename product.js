b8167a8119c196b3b4f22038414cd926ea31e618
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-07-18 10:44:33 +0200

    first commit

---
+++ product.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink: [
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721029913/1_nt0ww0.jpg",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721029913/2_am1lnc.jpg",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721029916/3_c5gfo2.jpg",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721029913/4_a2esne.jpg",
+    ],
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink: [
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281178/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_1_ooprer.jpg",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281180/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_whabr1.jpg",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281196/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_t38p9g.png",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721029913/1_nt0ww0.jpg",
+    ],
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+    imageLink: [
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281156/16480232420809ac54cac58a758d88fbee070f7f7a_thumbnail_900x_busob5.webp",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281157/1648023235ba32327d7aecda5b9de9913efa316d9e_thumbnail_900x_aysmxz.webp",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281175/164802324294a9e00cd18e3bdacdb07faadc6ea08e_thumbnail_900x_ek4yit.webp",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281178/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_1_ooprer.jpg",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281180/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_whabr1.jpg",
+    ],
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink: [
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281150/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Khaki___XL_uexmgz.png",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281157/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_White___XL_h42pn1.png",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281177/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Black___XL_mv8lch.png",
+      "https://res.cloudinary.com/dfkn5xnul/image/upload/v1721281190/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Grey___XL_hex1ey.png",
+    ],
+  },
+];
+module.exports = products;
